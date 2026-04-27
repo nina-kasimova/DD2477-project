92fcb253df56e8cc90cb9be9a7c7b171c3052de5
nina <nina.kasimova@gmail.com>
2026-04-27 13:14:59 +0200

    change keys

--- elasticAPI.py
+++ elasticAPI.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch, helpers
 client = Elasticsearch(
-    "https://my-elasticsearch-project-b7af22.es.us-central1.gcp.elastic.cloud:443",
-    api_key="RDdDRmdwMEI0dlNRLXQyRWhtRVY6UmpFeDNmRGV0dXB4Wk4yb0Fwc25TZw=="
+    "https://my-elasticsearch-project-add782.es.us-central1.gcp.elastic.cloud:443",
+    api_key="QWhXaXpwMEJOSEV0eXRtOG55VXM6a0FqdjJ6ODRIN3lxLXAwcXVtMmFvUQ=="
 )
 # -----------------------------
 # Define the index name and create index (if it doesn't exist)
